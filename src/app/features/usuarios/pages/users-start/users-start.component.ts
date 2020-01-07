e8f9f905950750a635a8e980c5076a838a9d44a8
Renzo Alvarado <ralvarado@safetypay.com>
2020-01-07 18:00:17 -0500

    added tabs

--- src/app/features/usuarios/pages/users-start/users-start.component.ts
+++ src/app/features/usuarios/pages/users-start/users-start.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/features/shared/services/users.service';
 
+import { clone } from 'ramda';
+
 @Component({
   selector: 'app-users-start',
   templateUrl: './users-start.component.html',
@@ -8,14 +10,19 @@ import { UsersService } from 'src/app/features/shared/services/users.service';
 })
 export class UsersStartComponent implements OnInit {
 
-  arr;
+  userArr;
   constructor(
     private userServ:UsersService,
   ) { }
 
   ngOnInit(): void {
-    this.userServ.getUsersList('GEZCON').subscribe((userData)=>{
-      this.arr = userData;
+    this.userServ.getUsersList('GEZCON').subscribe((userData:any[])=>{
+      this.userArr = clone(userData);
+      let counter = 0;
+      while(this.userArr.length < 5 || counter >8) {
+        counter++;
+        this.userArr.push({username:null})
+      }
       console.log("user data, ", userData)
     })
   }
